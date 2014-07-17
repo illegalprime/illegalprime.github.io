e9d005216d6a4feb265a43ffc6e4ebf75e0e517d
Michael Eden <themichaeleden@gmail.com>
2014-07-17 10:56:53 -0500

    Added hover animations to nav menu and a custom 404 page.

---
+++ assets/js/additions.js
@@ -0,0 +1,10 @@
+// Place for Javascript that will be put on the <head>
+// of every document.
+/*
+function highlight(x) {
+	x.innerHTML = "Hello!";
+}
+
+function normal(x) {
+	x.innerHTML = "OK";
+}*/
