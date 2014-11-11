f713d36b716784658d6df328ef834a2c3bf45345
Michael Eden <themichaeleden@gmail.com>
2014-11-11 16:51:06 -0500

    Added better mobile menu

--- assets/js/additions.js
+++ assets/js/additions.js
@@ -8,3 +8,22 @@ function highlight(x) {
 function normal(x) {
 	x.innerHTML = "OK";
 }*/
+
+var toggled = false;
+
+$(document).ready(function() {
+	$("#menu_btn").click(function() {
+		if (toggled) {
+			$("#following_menu_container").css("display", "none");
+			$("#following_social_container").css("display", "none");
+			$("#following_menu").toggleClass("collapsed", true);
+		}
+		else {
+			$("#following_menu_container").css("display", "inherit");
+			$("#following_social_container").css("display", "inherit");
+			$("#following_menu").toggleClass("collapsed", false);
+		}
+		toggled = !toggled;
+		// console.log("Clicked");
+	});
+});
