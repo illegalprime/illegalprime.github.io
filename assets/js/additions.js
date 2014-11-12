0a3c54cab4e403254722c9eb5b24e1464f639020
Michael Eden <themichaeleden@gmail.com>
2014-11-11 19:30:58 -0500

    Mobile CSS and Animations

--- assets/js/additions.js
+++ assets/js/additions.js
@@ -10,20 +10,35 @@ function normal(x) {
 }*/
 
 var toggled = false;
+var menu;
+var social;
+var container;
 
 $(document).ready(function() {
+	menu = $("#following_menu_container");
+	social = $("#following_social_container");
+	container = $("#following_menu");
+
 	$("#menu_btn").click(function() {
-		if (toggled) {
-			$("#following_menu_container").css("display", "none");
-			$("#following_social_container").css("display", "none");
-			$("#following_menu").toggleClass("collapsed", true);
+		if (container.queue("fx").length == 0) {
+			if (toggled) {
+				menu.fadeOut(150);
+				social.fadeOut(150);
+				container.animate({
+						width: 60,
+						height: 60
+					}, 200);
+			}
+			else {
+				container.animate({
+					width: "100%",
+					height: "325"
+				}, 200, function() {
+					menu.fadeIn(150);
+					social.fadeIn(150);
+				});
+			}
+			toggled = !toggled;
 		}
-		else {
-			$("#following_menu_container").css("display", "inherit");
-			$("#following_social_container").css("display", "inherit");
-			$("#following_menu").toggleClass("collapsed", false);
-		}
-		toggled = !toggled;
-		// console.log("Clicked");
 	});
 });
