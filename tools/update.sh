f05d6b998414dc3ff5e654fd1bf95af1ae11f85c
Michael Eden <themichaeleden@gmail.com>
2016-12-22 20:58:39 -0500

    try to do travis part one

--- tools/update.sh
+++ tools/update.sh
@@ -2,25 +2,44 @@
 # Author michael <themichaeleden@gmail.com>
 set -euo pipefail
 
-OLD_BRANCH=$(git branch | grep '^*' | cut -d' ' -f2)
-COMMIT_MSG="$1"
+PWD=$(pwd)
+export REV=$(git rev-parse --short HEAD)
+export REMOTE="https://$GH_TOKEN@github.com/illegalprime/illegalprime.github.io.git"
+export N_PREFIX="$PWD/node_modules/.bin/nodes"
 
-JEKYLL_ENV=production jekyll build -d /tmp/_site
+# update node
+n="$PWD/node_modules/.bin/n"
+npm install n
+$n lts
+node="$N_PREFIX/bin/node"
+npm="$N_PREFIX/bin/npm"
 
-git co master
+$node $npm install
+cleancss="$PWD/node_modules/.bin/cleancss"
+uncss="$PWD/node_modules/.bin/uncss"
 
-git fetch
+gem install jekyll
+bundle install
+bundle exec jekyll build
 
-git reset --hard origin/master
+cd _site
 
-git clean -d -f
+# process css
+echo "Processing CSS..."
+find -type f -name '*.html' | \
+    grep -v -f ../exclude | \
+    xargs $node $uncss -H . -s /assets/css/main.css | \
+    $node $cleancss \
+    > assets/css/main.min.css
+mv assets/css/main.min.css assets/css/main.css
 
-rsync -auP --checksum /tmp/_site/ "$(pwd)"
-
-git add -A
-
-git commit -m "$COMMIT_MSG"
-
-git push origin master
-
-git co "$OLD_BRANCH"
+git init
+git config user.name 'Michael Eden'
+git config user.email 'themichaeleden@gmail.com'
+git remote add upstream "$REMOTE"
+git fetch upstream
+git reset upstream/master
+git status
+git add -A .
+git commit -m "Automatic deploy of website for commit $REV"
+git push -q upstream HEAD:master
