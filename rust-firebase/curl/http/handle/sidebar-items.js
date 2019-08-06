ccb8e15982fcd9045693833048b1efb34ac3259a
Michael Eden <themichaeleden@gmail.com>
2019-08-06 09:06:54 -0400

    rust-firebase: remove docs, go to docs.rs

--- rust-firebase/curl/http/handle/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Handle",""],["Request",""]],"trait":[["ToUrl",""]],"enum":[["Method",""]]});
--- rust-firebase/curl/http/response/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"type":[["Headers",""]],"struct":[["Response",""]]});
