fb41f694acdd5cd3a29123ecbe3b97a309b49c27
Michael Eden <themichaeleden@gmail.com>
2021-01-14 13:36:17 -0500

    game-chooser: +co2, +detective, +unlock, +honey buzz, +robinson crusoe, +rap godz, +spirit island

--- ci/bgg/missing_games.sh
+++ ci/bgg/missing_games.sh
@@ -6,6 +6,13 @@ BGG_YAML="${2:-$DATA_DIR/bgg_data.yaml}"
 MY_YAML="${3:-$DATA_DIR/my_data.yaml}"
 
 ALL=$(yq -r 'keys[]' < "$BGG_YAML")
-ENTERED=$(yq -r 'to_entries[] | select(.value.tags == null) | .key' < "$MY_YAML")
 
-comm -1 <(sort -u <<< "$ALL") <(sort -u <<< "$ENTERED")
+# entered games
+ENTERED=$(yq -r 'to_entries[] | .key' < "$MY_YAML")
+# hidden
+HIDDEN=$(yq -r 'to_entries[] | select(.value.tags == null) | .key' < "$MY_YAML")
+# remove hidden from entered
+VISIBLE=$(comm -23 <(sort -u <<< "$ENTERED") <(sort -u <<< "$HIDDEN"))
+# show only games that appear in bgg and not in my own data
+echo -e 'To Add\tTo Remove\n'
+comm -3 <(sort -u <<< "$ALL") <(sort -u <<< "$VISIBLE")
