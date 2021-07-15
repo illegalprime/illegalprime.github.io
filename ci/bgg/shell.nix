e2059b41e33802e9b5e4636a381e48469c493399
Michael Eden <themichaeleden@gmail.com>
2021-07-15 13:55:01 -0400

    pin bgg nix derivation

--- ci/bgg/shell.nix
+++ ci/bgg/shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchGit {
+  name = "nixos-unstable-2018-09-12";
+  url = https://github.com/nixos/nixpkgs/;
+  rev = "b58ada326aa612ea1e2fb9a53d550999e94f1985";
+}) {} }:
 
 pkgs.stdenv.mkDerivation {
   name = "bgg";
